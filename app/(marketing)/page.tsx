8419820e971ab22b80d1eb1eb26d38717e5c0910
Hugo Archundia <hugo@budly.ai>
2024-04-16 16:15:56 -0600

    Create Home page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,17 @@
-import { Button } from '@/components/ui/button'
+import Footer from './_components/Footer'
+import Heading from './_components/Heading'
+import Heroes from './_components/Heroes'
 
-const Home = () => {
+const MarketingPage = () => {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className='min-h-full flex flex-col'>
+      <div className='flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10'>
+        <Heading />
+        <Heroes />
+      </div>
+      <Footer />
     </div>
   )
 }
 
-export default Home
+export default MarketingPage
