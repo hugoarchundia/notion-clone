9d504f15c2f68e12553d372b80f1f2b67386855a
Hugo Archundia <hugo@budly.ai>
2024-04-21 00:20:17 -0600

    Refactor marketing components and add Convex authentication functionality

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -1,9 +1,15 @@
 'use client'
 
+import { Spinner } from '@/components/Spinner'
 import { Button } from '@/components/ui/button'
+import { SignInButton } from '@clerk/clerk-react'
+import { useConvexAuth } from 'convex/react'
 import { ArrowRight } from 'lucide-react'
+import Link from 'next/link'
 
 const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
+
   return (
     <div className='max-w-3xl space-y-4'>
       <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold'>
@@ -14,10 +20,27 @@ const Heading = () => {
         Jotion is the connected workspace where <br />
         better, faster work happens
       </h3>
-      <Button>
-        Enter Jotion
-        <ArrowRight className='h-4 w-4 ml-2' />
-      </Button>
+      {isLoading && (
+        <div className='flex justify-center items-center'>
+          <Spinner size='lg' />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href='/documents'>
+            Enter Jotion
+            <ArrowRight className='h-4 w-4 ml-2' />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode='modal'>
+          <Button size='lg'>
+            Get Jotion Free
+            <ArrowRight className='h-4 w-4 ml-2' />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   )
 }
--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,11 +1,17 @@
 'use client'
 
+import { Spinner } from '@/components/Spinner'
 import ToggleTheme from '@/components/ToggleTheme'
+import { Button } from '@/components/ui/button'
 import useScrollTop from '@/hooks/useScrollTop'
 import { cn } from '@/lib/utils'
+import { SignInButton, UserButton } from '@clerk/clerk-react'
+import { useConvexAuth } from 'convex/react'
+import Link from 'next/link'
 import Logo from './Logo'
 
 const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
   const scrolled = useScrollTop()
 
   return (
@@ -17,6 +23,34 @@ const Navbar = () => {
     >
       <Logo />
       <div className='md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2'>
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode='modal'>
+              <Button
+                variant='ghost'
+                size='sm'
+              >
+                Log In
+              </Button>
+            </SignInButton>
+            <SignInButton mode='modal'>
+              <Button size='sm'>Get Jotion Free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button
+              variant='ghost'
+              size='sm'
+              asChild
+            >
+              <Link href='/documents'>Enter Jotion</Link>
+            </Button>
+            <UserButton />
+          </>
+        )}
         <ToggleTheme />
       </div>
     </div>
